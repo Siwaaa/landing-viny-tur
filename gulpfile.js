7cd443acbd4e1ee055d895fe3589b883113b6c45
Siwaaa <siwaa@mail.ru>
2021-07-04 20:10:33 +0300

    Сборка gulp шаблона

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,73 @@
-const {src, dest} = require('gulp')
+const {src, dest, series, parallel, watch} = require('gulp')
 const sass = require('gulp-sass')
 const csso = require('gulp-csso')
+const htmlmin = require('gulp-htmlmin')
+const include = require('gulp-file-include')
+const babel = require('gulp-babel')
+const replace = require('gulp-replace')
+const del = require('del')
+const sync = require('browser-sync')
+const autoPrefixer = require('gulp-autoprefixer')
+
+// HTML
+
+function html() {
+  return src('src/*.html')
+    .pipe(include({
+      prefix: '@'
+    }))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true
+    }))
+    .pipe(dest('build'))
+    .pipe(sync.stream())
+}
+
+// STYLES
+
+function styles() {
+  return src('src/css/*.css')
+    .pipe(autoPrefixer({
+      overrideBrowserslist: ['last 5 versions'],
+      cascade: false
+    }))
+    .pipe(csso())
+    .pipe(dest('build/css/'))
+    .pipe(sync.stream())
+}
+
+// SCRIPTS
+
+function scripts() {
+  return src('src/js/*.js')
+    .pipe(babel())
+    .pipe(dest('build/js/'))
+    .pipe(sync.stream())
+}
+
+function clean() {
+  return del('build')
+}
+
+function serve() {
+  sync.init({
+    server: './build'
+  })
+
+  watch(['src/*.html', 'src/blocks/*.html'], html)
+  watch('src/css/*.css', styles)
+  watch('src/js/*.js', scripts)
+}
+
+// ** EXPORTS **
+exports.build = series(
+  clean,
+  parallel(html, styles, scripts)
+)
+
+exports.serve = series(
+  clean,
+  parallel(html, styles, scripts),
+  serve
+)
