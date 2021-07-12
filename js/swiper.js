8a98cb4aa7bb25ca00c209fc8242e4ac8ef5d256
Siwaaa <siwaa@mail.ru>
2021-07-12 16:03:01 +0300

    Релиз без паралакса

--- js/swiper.js
+++ js/swiper.js
@@ -3,8 +3,9 @@ const swiper = new Swiper('.programs__carusel', {
   slidesPerView: 1,
   spaceBetween: 30,
   CSSWidthAndHeight: true,
+  preloadImages: false,
   loop: true,
-  grabCursor: true,
+  // grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true
@@ -18,6 +19,15 @@ const swiper2 = new Swiper('.swiper-slide__img ', {
   // Optional parameters
   slidesPerView: 1,
   spaceBetween: 30,
+  CSSWidthAndHeight: true,
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: true
+  },
   loop: true,
-  nested: true
+  nested: true,
+  navigation: {
+    nextEl: ".programs-in-swiper__next",
+    prevEl: ".programs-in-swiper__prev"
+  }
 });
